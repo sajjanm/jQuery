aa9dfed6cc6d4cf0dfba47e94089a4d3e1bb2b46
Sajjan Mishra <Sajjan Mishra>
2015-12-12 01:32:08 +0545

    hello

---
+++ assets/js/models/student.model.js
@@ -0,0 +1,14 @@
+function Student(){
+	this.id=0;
+	this.firstName="";
+	this.lastName="";
+	this.email="";
+	this.contactNo="";
+	this.status=false;
+
+	this.getFullName=function(){
+		return (this.firstName+" "+this.lastName);
+	}
+
+
+}
---
+++ assets/js/services/student.service.js
@@ -0,0 +1,12 @@
+function StudentService(){
+	this.stdList=[];
+
+	this.add=function(student){
+		this.stdList.push(student);
+	}
+
+	this.getAll=function(){
+		return this.stdList;
+	}
+
+}
